7dc554acef7d8eff6d2a759cfba7e5b5e45a6586
dmaupin <dm@davidmaupin.com>
2015-01-23 21:49:19 -0800

    Fonts :(x2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-    config.action_controller.asset_host = "https://s3.amazonaws.com/kanpai-assets/.cloudfront.net"
+    config.action_controller.asset_host = "<https://s3.amazonaws.com/kanpai-assets/"
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
