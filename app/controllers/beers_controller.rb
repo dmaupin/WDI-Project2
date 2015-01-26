7f32994943dc45b114ca325e2062f697c5b9620e
dmaupin <dm@davidmaupin.com>
2015-01-25 19:32:42 -0800

    Fixed Pagination order

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -5,7 +5,7 @@ class BeersController < ApplicationController
     def index
       if current_user
         # @beers = current_user.beers.all
-        @beers = current_user.beers.paginate(:page => params[:page], :per_page => 6)
+        @beers = current_user.beers.all.order(created_at: :desc).paginate(:page => params[:page], :per_page => 6)
       end
     end
 
